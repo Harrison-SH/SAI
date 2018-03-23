d89dc85ff7b0ba42bf051daa53762952c89aa2d8
krishna <krishna@barefootnetworks.com>
2018-03-22 17:21:43 -0700

    move bridge stats to the end for backward compatibility

--- inc/saibridge.h
+++ inc/saibridge.h
@@ -495,12 +495,12 @@ typedef struct _sai_bridge_api_t
     sai_remove_bridge_fn                remove_bridge;
     sai_set_bridge_attribute_fn         set_bridge_attribute;
     sai_get_bridge_attribute_fn         get_bridge_attribute;
-    sai_get_bridge_stats_fn             get_bridge_stats;
-    sai_clear_bridge_stats_fn           clear_bridge_stats;
     sai_create_bridge_port_fn           create_bridge_port;
     sai_remove_bridge_port_fn           remove_bridge_port;
     sai_set_bridge_port_attribute_fn    set_bridge_port_attribute;
     sai_get_bridge_port_attribute_fn    get_bridge_port_attribute;
+    sai_get_bridge_stats_fn             get_bridge_stats;
+    sai_clear_bridge_stats_fn           clear_bridge_stats;
     sai_get_bridge_port_stats_fn        get_bridge_port_stats;
     sai_clear_bridge_port_stats_fn      clear_bridge_port_stats;
 } sai_bridge_api_t;
